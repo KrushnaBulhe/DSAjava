a3a68f8e60a0c27e3de02e04168f76907bf8e122
KrushnaBulhe <166788580+KrushnaBulhe@users.noreply.github.com>
2024-04-13 00:05:31 +0530

    Add files via upload

---
+++ areacrl.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+
+public class areacrl {
+    public static void main(String[] args) {
+        System.out.println("enter the value of radius ::");
+        Scanner scan = new Scanner(System.in);
+        float rad = scan.nextInt();
+        float area = 3.14f * rad * rad;
+        System.out.println(area);
+
+    }
+}
